7a15915e1873fbf054d6fdae2203b3b5ac83f218
Diego Mero <diegomero_1999@hotmail.com>
2023-02-15 13:07:53 -0500

    Add categories page and their fucntionalities

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,25 @@
 class GroupsController < ApplicationController
+  def index
+    @groups = current_user.groups
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(form_params.merge(user: current_user, icon: 'Icon not available yet'))
+
+    if @group.save
+      redirect_to groups_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def form_params
+    params.require(:group).permit(:name)
+  end
 end
