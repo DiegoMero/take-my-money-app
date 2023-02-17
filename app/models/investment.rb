d0f54496f89feb592df97115c254418aeeacf22e
Diego Mero <diegomero_1999@hotmail.com>
2023-02-16 23:21:45 -0500

    Add validations

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,4 +1,6 @@
 class Investment < ApplicationRecord
   has_and_belongs_to_many :groups
   belongs_to :user
+
+  validates :name, :amount, :groups, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :investments
   has_many :groups
+
+  validates :name, presence: true
 end
